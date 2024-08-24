3940ac8a1a22d8b7b52ef9d76766db44c2d55167
msadeedashraf <157824309+msadeedashraf@users.noreply.github.com>
2024-08-23 21:41:23 -0400

    .

--- day10/grocerieslist/src/ItemList.js
+++ day10/grocerieslist/src/ItemList.js
@@ -7,7 +7,6 @@ const ItemList = ({ items, handleCheck, handleDelete }) => {
     <ul>
       {items.map((item) => (
         <LineItem
-          key={item.id}
           item={item}
           handleCheck={handleCheck}
           handleDelete={handleDelete}
